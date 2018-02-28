973425968b9afc0a23ec7225586361a97dd96379
schevalier <chevalier.syl20@gmail.com>
2018-02-28 19:44:46 +0100

    Updated deployment recipe

--- deploy.sh
+++ deploy.sh
@@ -9,18 +9,16 @@ then
 fi
 
 echo "Deleting old publication"
+
 rm -rf public
-mkdir public
-git worktree prune
-rm -rf .git/worktrees/public/
 
 echo "Checking out gh-pages branch into public"
+
+git worktree prune
 git worktree add -B gh-pages public origin/gh-pages
 
-echo "Removing existing files"
-rm -rf public/*
-
 echo "Generating site"
+
 hugo
 
 echo "Updating gh-pages branch"
